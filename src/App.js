eaa36eba210c4b271c134e2d5cc42c19d3c35aad
Mohamed Elwaghef <mohamed.elwaghef@sodexo.com>
2019-11-19 13:36:39 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import Recipe from './Recipe';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+
+  const APP_ID = '9e8040d6';
+  const APP_KEY = "e2a9c08d32adfcd78361ff005b8acdef";
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
+  // const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  // const [counter, setCounter] = useState(0);
+  useEffect( () => {
+    // console.log("effect has been run!");
+    getRecepices([search]);
+
+  }, [query]); 
+
+
+  const getRecepices = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    console.log(data.hits);
+    setRecipes(data.hits);
+  }
+
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+  }
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form">
+          <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
+          <button className="search-bar" type="submit">Search</button>
+      </form>
+
+      <div className="recipes">
+        {recipes.map(recipe => (
+          <Recipe key={recipe.recipe.label}
+                  title={recipe.recipe.label} 
+                  calories={recipe.recipe.calories}
+                  image={recipe.recipe.image}
+                  ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
+
+      {/* <h1 onClick={() => setCounter(counter+1)}>{counter}</h1> */}
     </div>
+
+
   );
 }
 
